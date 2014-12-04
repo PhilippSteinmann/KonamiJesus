21f80dc2b37f2af227c15b30e131e4a0d8e72e6b
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-12-03 22:36:17 -0500

    Minor stuff + gitignore

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener("keydown", checkPress);
+});
+
+var fkeys=[];
+var okeys=[74, 69,83,85,83,13];
+
+function checkPress(k) {
+    clear();
+    if (fkeys.length > okeys.length){
+        fkeys=[];    
+    }
+    fkeys.push(k.keycode);
+    console.log(fkeys);
+    if (fkeys.join("")==okeys.join("")){
+        superImposeJesus();
+    }
+}
+
 var jesus_url = "http://www.catholictradition.org/Classics/heart-jesus1.jpg";
 
 function superImposeJesus() {
@@ -8,18 +27,4 @@ function superImposeJesus() {
     $("body").append(imageElement);
 
     $(".jesus-image").fadeIn(2000);
-}
-
-var fkeys=[];
-var okeys=[74, 69,83,85,83,13];
-window.addEventListener("keydown",checkPress);
-var checkPress=function (k){
-    clear();
-    if (fkeys.length > okeys.length){
-        fkeys=[];    
-    }
-    fkeys.push(k.keycode);
-    if (fkeys.join("")==okeys.join("")){
-        superImposeJesus();
-    }
 }

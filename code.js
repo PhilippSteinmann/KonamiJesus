d6a831782a29b0d06186dfae6a2ff9d9006e0660
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-12-03 22:25:37 -0500

    Add Jesus code

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
+var jesus_url = "http://www.catholictradition.org/Classics/heart-jesus1.jpg";
+
+function superImposeJesus() {
+    var imageElement = document.createElement("img");
+    imageElement.className = "jesus-image";
+    imageElement.src = jesus_url;
+
+    $("body").append(imageElement);
+
+    $(".jesus-image").fadeIn(2000);
+}
+
 var fkeys=[];
 var okeys=[74, 69,83,85,83,13];
 window.addEventListener("keydown",checkPress);
@@ -8,6 +20,6 @@ var checkPress=function (k){
     }
     fkeys.push(k.keycode);
     if (fkeys.join("")==okeys.join("")){
-        //do Jesus stuff
+        superImposeJesus();
     }
 }
